12fb572676bae2f1d528476ac80736069c3dc4aa
barneygale <barney.gale@gmail.com>
2023-12-22 20:28:36 +0000

    Remove sync script + patch

--- sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-cp cpython/Lib/pathlib/_abc.py                       pathlib_abc/__init__.py
-cp cpython/Lib/test/test_pathlib/test_pathlib_abc.py tests/test_pathlib_abc.py
-
-patch -p1 < sync.patch
